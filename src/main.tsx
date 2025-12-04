be5650ec559b8e2ef49d543b771d7d10fa8df92e
Juan Trejos <jctrejosi@unal.edu.co>
2025-12-04 18:21:47 -0500

    feat: update profile images and background assets; enhance Home page translations and styles
    - Updated profile image to a new version.
    - Added new background images for the Home page.
    - Enhanced translations for the hero section with additional details.
    - Refactored Home component to utilize new assets and improved styling.
    - Adjusted CSS variables for better color management and consistency.

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
+import { router } from '@/routes'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
-import { router } from '@/routes'
 
 import '@/styles/index.css'
 
